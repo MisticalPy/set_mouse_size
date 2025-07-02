18d244e3f3c7a1414e3a407ff29252e92d968509
Gr0m41k <alexapakalips@gmail.com>
2025-07-02 22:05:33 +0700

    изменил способ запуска меню параметры через сочетание клавиш win + i работает

--- size_mouse.py
+++ size_mouse.py
@@ -2,8 +2,9 @@ from time import sleep
 
 import keyboard
 from mouse import *
-position_big_mouse = [(807, 1055), (21, 917), (124, 574), (762, 326), (1428, 397), (1895, 9)]
-position_small_mouse = [(807, 1053), (23, 911), (142, 575), (802, 314), (1341, 398), (1897, 21)]
+
+position_big_mouse = [(124, 574), (762, 326), (1428, 397), (1895, 9)]
+position_small_mouse = [(142, 575), (802, 314), (1341, 398), (1897, 21)]
 
 position_in_this_time = get_position()
 position_big_mouse.append(position_in_this_time)
@@ -15,12 +16,14 @@ flag = True
 def mouse_set_size(position_big_or_small_mouse):
     for position_x, position_y in position_big_or_small_mouse:
         move(position_x, position_y, absolute=True, duration=0)
-        sleep(1.1)
+        sleep(1.4)
         if position_x != position_big_or_small_mouse[-1][0] and position_y != position_big_or_small_mouse[-1][1]:
             click()
 
 
 def toggle_mouse_size():
+    sleep(1)
+    keyboard.send('win+i')
     global flag
     if flag:
         flag = False
@@ -30,7 +33,7 @@ def toggle_mouse_size():
         mouse_set_size(position_small_mouse)
 
 
-keyboard.add_hotkey("alt", toggle_mouse_size)
+keyboard.add_hotkey("alt+m", toggle_mouse_size)
 try:
     keyboard.wait()
 except KeyboardInterrupt:

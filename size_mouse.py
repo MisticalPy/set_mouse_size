846343ecec32ecaca239667dfec55e6c14d81ffc
Gr0m41k <alexapakalips@gmail.com>
2025-07-02 20:39:09 +0700

    теперь курсор после увеличение еще и возвращается на то место где он был изначально

--- size_mouse.py
+++ size_mouse.py
@@ -2,9 +2,13 @@ from time import sleep
 
 from mouse import *
 
-position_big_mouse = [(805, 1053), (21, 918), (175, 574), (784, 323), (1428, 398)]
+position_big_mouse = [(805, 1053), (21, 918), (175, 574), (784, 323), (1428, 398), (1806, 12)]
+
+position_in_this_time = get_position()
+position_big_mouse.append(position_in_this_time)
 
 for position_x, position_y in position_big_mouse:
     move(position_x, position_y, absolute=True, duration=0)
     sleep(0.3)
-    click()
+    if position_x != position_big_mouse[-1][0] and position_y != position_big_mouse[-1][1]:
+        click()

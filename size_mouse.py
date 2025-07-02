43c5b12147eaf0a9557cd35cc50bb17b07a11050
Gr0m41k <alexapakalips@gmail.com>
2025-07-02 21:47:57 +0700

    готовая версия кода

--- size_mouse.py
+++ size_mouse.py
@@ -2,9 +2,8 @@ from time import sleep
 
 import keyboard
 from mouse import *
-
-position_big_mouse = [(832, 1055), (22, 917), (135, 574), (762, 326), (1428, 397), (1895, 9)]
-position_small_mouse = [(832, 1053), (23, 911), (142, 575), (802, 314), (1341, 398), (1897, 21)]
+position_big_mouse = [(807, 1055), (21, 917), (124, 574), (762, 326), (1428, 397), (1895, 9)]
+position_small_mouse = [(807, 1053), (23, 911), (142, 575), (802, 314), (1341, 398), (1897, 21)]
 
 position_in_this_time = get_position()
 position_big_mouse.append(position_in_this_time)
@@ -16,21 +15,23 @@ flag = True
 def mouse_set_size(position_big_or_small_mouse):
     for position_x, position_y in position_big_or_small_mouse:
         move(position_x, position_y, absolute=True, duration=0)
-        sleep(1.2)
+        sleep(1.1)
         if position_x != position_big_or_small_mouse[-1][0] and position_y != position_big_or_small_mouse[-1][1]:
             click()
 
 
+def toggle_mouse_size():
+    global flag
+    if flag:
+        flag = False
+        mouse_set_size(position_big_mouse)
+    else:
+        flag = True
+        mouse_set_size(position_small_mouse)
+
+
+keyboard.add_hotkey("alt", toggle_mouse_size)
 try:
-    while True:
-        sleep(1)
-        if keyboard.is_pressed('alt'):
-            sleep(0.5)
-            if flag:
-                flag = False
-                mouse_set_size(position_big_mouse)
-            else:
-                flag = True
-                mouse_set_size(position_small_mouse)
+    keyboard.wait()
 except KeyboardInterrupt:
-    print('end')
+    print('Interrupted')

37a14397abdbb06f2e7294e3922a4858316df107
Gr0m41k <alexapakalips@gmail.com>
2025-07-02 20:24:46 +0700

    опредение позиции нажатия левой кнопки + запись сразу в список и вывод этого списка

--- size_mouse.py
+++ size_mouse.py
@@ -1,1 +1,16 @@
+from time import sleep
+
 from mouse import *
+
+
+position_big_mouse = []
+
+on_click(lambda: position_big_mouse.append(get_position()))
+on_click(lambda: print(position_big_mouse))
+
+
+try:
+    while True:
+        sleep(1)
+except KeyboardInterrupt:
+    print('Ctrl+C pressed')
